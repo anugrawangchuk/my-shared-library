6e0bae5522eac6985aa4633dc0a281793c5f69df
Anugra Wangchuk Lepcha <106028390+anugrawangchuk@users.noreply.github.com>
2024-08-28 16:03:46 +0530

    Update ansible_check.groovy

--- vars/ansible_check.groovy
+++ vars/ansible_check.groovy
@@ -1,5 +1,5 @@
 def call() {
     stage('Clone Repository') {
-        git branch: 'main', url: 'https://github.com/OT-MyGurukulam/Ansible_27.git'
+        git branch: 'main', url: 'https://github.com/anugrawangchuk/Ansible.git'
     }
 }
