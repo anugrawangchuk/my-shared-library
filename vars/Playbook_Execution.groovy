e42d39957bcbdb8c73c8577a2c4cf531c52ec7c2
Anugra Wangchuk Lepcha <106028390+anugrawangchuk@users.noreply.github.com>
2024-08-28 16:07:51 +0530

    Update Playbook_Execution.groovy

--- vars/Playbook_Execution.groovy
+++ vars/Playbook_Execution.groovy
@@ -1,6 +1,6 @@
 def call() {
     stage('playbook execution') {
 
-        sh "ansible-playbook playbook.yml -i inventory.ini"
+        sh "ansible-playbook Ansible/playbook.yml -i Ansible/inventory.ini"
     }
 }
